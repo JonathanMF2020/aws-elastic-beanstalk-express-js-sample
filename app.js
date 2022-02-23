5242848a85d583cd4a826f19730ab4e7fbd719b5
Jonathan Mojica <jonathanmojicafernandez@gmail.com>
2022-02-22 21:43:33 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola Mundo Jonathan Mojica Fernandez!'));
+app.get('/', (req, res) => res.send('Hola Mundo AWS de Jonathan Mojica Fernandez'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
